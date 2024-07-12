f51cd3792dd07f49087ecec8715188d465820400
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-07-12 22:39:02 +0700

    Initial Commit

---
+++ Assets/Art/Textures/Pages/5/Anak A/spritecutter.py
@@ -0,0 +1,23 @@
+# get all image ends with .png or .PNG or .jpg or .JPG or >jpeg or .JPEG
+# and cut them from 0,0 to 2048,2048
+# and save them to a new folder named "cutted"
+
+import os
+from PIL import Image
+
+def cut_image(image_path, left, upper, right, lower):
+    img = Image.open(image_path)
+    img = img.crop((left, upper, right, lower))
+    return img
+
+def main():
+    if not os.path.exists("cutted"):
+        os.makedirs("cutted")
+
+    for file in os.listdir("."):
+        if file.endswith(".png") or file.endswith(".PNG") or file.endswith(".jpg") or file.endswith(".JPG") or file.endswith(".jpeg") or file.endswith(".JPEG"):
+            img = cut_image(file, 0, 0, 2048, 2048)
+            img.save("cutted/" + file)
+
+if __name__ == "__main__":
+    main()
